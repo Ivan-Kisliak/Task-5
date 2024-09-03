e0f67be06bdd1f6820647e57f63c75bffed3c401
Ivan <ivangodno86@gmail.com>
2024-09-03 16:11:37 +0300

    Add property name and function print info

---
+++ TaskForLesson2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TaskForLesson2
+//
+//  Created by  Ivan Kiskyak on 3.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let name = "Иван"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Меня зовут \(name), я создал свое первое приложение")
+    }
+
+}
+
