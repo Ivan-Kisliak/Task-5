d13e076bc59f108c6924a2fc25a011f70c367d71
Ivan <ivangodno86@gmail.com>
2024-09-07 17:52:59 +0300

    Add class Person

---
+++ TaskForLesson2/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  TaskForLesson2
+//
+//  Created by  Ivan Kiskyak on 7.09.24.
+//
+
+import Foundation
+
+class Person {
+    
+    let name = "Иван"
+    let surname = "Кисляк"
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
--- TaskForLesson2/ViewController.swift
+++ TaskForLesson2/ViewController.swift
@@ -8,17 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let name = "Иван"
-    let surname = "Кисляк"
-    
-    var fullName: String {
-        "\(name) \(surname)"
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Меня зовут \(fullName), я создал свое второе приложение")
         
     }
 }
