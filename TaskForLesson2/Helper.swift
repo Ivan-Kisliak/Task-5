85f105555bf55035973aa4d9fe4e35433dac4744
Ivan <ivangodno86@gmail.com>
2024-09-18 14:18:40 +0300

    Add randomUserLabel and ShowFullNameButton in ViewController

--- TaskForLesson2/Helper.swift
+++ TaskForLesson2/Helper.swift
@@ -11,9 +11,9 @@ class Helper {
     
     private var users: [User] = []
     
-    func addUser(_ user: User) { // В задании не указано надо ли удалять этот
-        users.append(user)       // метод :) Поэтому оставил его, но мне кажется
-    }                            // что оставлять неиспользуемый метод не очень.
+    func addUser(_ user: User) {
+        users.append(user)
+    }
     
     func addUsers(_ users: [User]) {
         self.users.append(contentsOf: users)
@@ -22,6 +22,11 @@ class Helper {
     func getUsers() -> [User] {
         users
     }
+    
+    func getRandomUser() -> User? {
+        users.randomElement()
+    }
+    
 }
 
 
--- TaskForLesson2/ViewController.swift
+++ TaskForLesson2/ViewController.swift
@@ -11,6 +11,16 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let userRepository = UserRepository()
+    
+    private let randomUserLabel = UILabel()
+    
+    private let showFullNameButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .green
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +38,22 @@ class ViewController: UIViewController {
     private func printPersonFullName() {
         helper.getUsers().forEach { print($0.personInfo.fullName) }
     }
+    
+    private func setupRandomUserLabel() {
+        let randomUser = helper.getRandomUser()?.personInfo.fullName
+        randomUserLabel.text = randomUser
+        randomUserLabel.font = UIFont.systemFont(ofSize: 25)
+        randomUserLabel.textColor = .blue
+        randomUserLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+    }
        
     private func setupView() {
-        view.backgroundColor = .systemGreen
-        view.alpha = 0.1
+        view.backgroundColor = .lightGray
+//        view.alpha = 0.1 //убрал чтобы видеть label
+        
+        setupRandomUserLabel()
+        view.addSubview(randomUserLabel)
+        view.addSubview(showFullNameButton)
     }
     
 }
